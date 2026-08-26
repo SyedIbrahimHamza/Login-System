f4d7efd5377d3f91ba2df4ba678af1a618e3a171
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-26 11:27:09 -0700

    add menu

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+#login system
 users = {}
+
+print("Welcome to the login system!")
+print("Please choose an option:")
+print("1. Register")
+print("2. Login")
+choice = input("Enter your choice (1 or 2): ")
+
