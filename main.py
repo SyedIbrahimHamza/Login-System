a0dab3e3fa87d1f04aae9a78c1e937bcb0dda285
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-26 11:31:03 -0700

    add a while loop

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ print("Please choose an option:")
 print("1. Register")
 print("2. Login")
 choice = input("Enter your choice (1 or 2): ")
-
+while choice not in ['1', '2']:
+    print("invalid choice. Please try again.")
+    choice = input("Enter your choice (1 or 2): ")
