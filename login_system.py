d0162b1cc6786530ab13e016d642e6260a402130
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-27 09:37:26 -0700

    change file name

---
+++ login_system.py
@@ -0,0 +1,17 @@
+users = {}
+
+print("Welcome to the login system!")
+print("Please choose an option:")
+print("1. Register")
+print("2. Login")
+choice = input("Enter your choice (1 or 2): ")
+while choice not in ['1', '2']:
+    print("invalid choice. Please try again.")
+    choice = input("Enter your choice (1 or 2): ")
+if choice == '1':
+    username = input("Enter a username: ")
+    while username in users:
+        print("Username already exists. Please try again.")
+        username = input("Enter a username: ")
+    password = input("Enter a password: ")
+    users[username] = password
