cded8bff11d84ab8ba71bad98681a7af2c33628f
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-28 12:16:46 -0700

    add a conditional statement for choice 2

--- login_system.py
+++ login_system.py
@@ -15,3 +15,10 @@ if choice == '1':
         username = input("Enter a username: ")
     password = input("Enter a password: ")
     users[username] = password
+if choice == '2':
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if username in users and users[username] == password:
+        print("Login successful!")
+    else:
+        print("Invalid username or password.")
